ec48ace96a6ec0d71a4df77d5b327ebba92b5f5a
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-23 21:16:47 +0700

    Completed prelim analyses

--- src/customer_segmentation_example/core.clj
+++ src/customer_segmentation_example/core.clj
@@ -21,9 +21,9 @@
 
 (def log-spending
   (-> invoices
-      (ml/transform (ml/tokeniser {:input-col :description
+      (ml/transform (ml/tokeniser {:input-col  :description
                                    :output-col :descriptors}))
-      (ml/transform (ml/stop-words-remover {:input-col :descriptors
+      (ml/transform (ml/stop-words-remover {:input-col  :descriptors
                                             :output-col :cleaned-descriptors}))
       (g/with-column :descriptor (g/explode :cleaned-descriptors))
       (g/with-column :descriptor (g/regexp-replace :descriptor
@@ -39,11 +39,63 @@
       (g/order-by (g/desc :log-spend))
       g/cache))
 
+(def pipeline
+  (ml/pipeline
+    (ml/string-indexer {:input-col  :descriptor
+                        :output-col :descriptor-id})
+    (ml/als {:max-iter    5
+             :reg-param   0.01
+             :rank        8
+             :nonnegative true
+             :user-col    :customer-id
+             :item-col    :descriptor-id
+             :rating-col  :log-spend})))
 
-;; TODO: pipeline + indexer
-(def model
-  (ml/fit log-spending (ml/als {:max-iter   5
-                                :reg-param  0.01
-                                :user-col   :customer-id
-                                :item-col   :descriptor
-                                :rating-col :log-spend})))
+(def pipeline-model
+  (ml/fit log-spending pipeline))
+
+(def id->descriptor
+  (ml/index-to-string {:input-col  :id
+                       :output-col :descriptor
+                       :labels (ml/labels (first (ml/stages pipeline-model)))}))
+
+;; TODO: vector literals
+;; TODO: .itemFactors .userFactors
+(def shared-patterns
+  (-> (.itemFactors (last (ml/stages pipeline-model)))
+      (ml/transform id->descriptor)
+      (g/select :descriptor (g/posexplode :features))
+      (g/rename-columns {:pos :pattern-id
+                         :col :factor-weight})
+      (g/with-column :pattern-rank
+                      (g/over (g/row-number)
+                              (g/window {:partition-by :pattern-id
+                                         :order-by     (g/desc :factor-weight)})))
+      (g/filter (g/< :pattern-rank 13))
+      (g/order-by :pattern-id (g/desc :factor-weight))
+      (g/select :pattern-id :descriptor :factor-weight)))
+
+
+(def customer-segments
+  (-> (.userFactors (last (ml/stages pipeline-model)))
+      (g/select (g/as :id :customer-id) (g/posexplode :features))
+      (g/rename-columns {:pos :pattern-id
+                         :col :factor-weight})
+      (g/with-column :customer-rank
+                      (g/over (g/row-number)
+                              (g/window {:partition-by :customer-id
+                                         :order-by     (g/desc :factor-weight)})))
+      (g/filter (g/= :customer-rank 1))))
+
+(-> shared-patterns
+    (g/group-by :pattern-id)
+    (g/agg {:descriptors (g/array-sort (g/collect-set "descriptor"))}) ;; TODO: change to keyword
+    (g/order-by :pattern-id)
+    g/show)
+
+(-> customer-segments
+    (g/group-by :pattern-id)
+    (g/agg {:n-customers (g/count-distinct :customer-id)})
+    (g/order-by :pattern-id)
+    g/show)
+
