fe40a27b8b38d729a78a9669e4cc5a05515bc3ee
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-27 13:53:41 +0700

    Added checkpointing

--- src/customer_segmentation_example/core.clj
+++ src/customer_segmentation_example/core.clj
@@ -7,7 +7,7 @@
 (defonce spark (g/create-spark-session {}))
 
 (def invoices
-  (-> (g/read-csv! spark "data/online_retail_ii")
+  (-> (g/read-csv! spark "/data/online_retail_ii")
       (g/select
         {:invoice      (g/int :Invoice)
          :stock-code   (g/int :StockCode)
@@ -17,6 +17,8 @@
          :price        (g/double :Price)
          :customer-id  (g/int "Customer ID")
          :country      :Country})))
+
+(g/count invoices)
 
 (def descriptors
   (-> invoices
@@ -32,6 +34,8 @@
                                                    (g/lit "")))
       (g/remove (g/< (g/length :descriptor) 3))))
 
+(g/show descriptors)
+
 (def log-spending
   (-> descriptors
       (g/remove (g/||
@@ -43,11 +47,13 @@
       (g/order-by (g/desc :log-spend))
       g/cache))
 
+(g/show log-spending)
+
 (def pipeline
   (ml/pipeline
     (ml/string-indexer {:input-col  :descriptor
                         :output-col :descriptor-id})
-    (ml/als {:max-iter    100
+    (ml/als {:max-iter    10
              :reg-param   0.01
              :rank        8
              :nonnegative true
@@ -80,6 +86,7 @@
       (g/select :pattern-id :descriptor :factor-weight)))
 
 
+;https://stackoverflow.com/questions/31484460/spark-gives-a-stackoverflowerror-when-training-using-als
 (def customer-segments
   (-> (ml/user-factors als-model)
       (g/select (g/as :id :customer-id) (g/posexplode :features))
@@ -91,17 +98,31 @@
                                          :order-by     (g/desc :factor-weight)})))
       (g/filter (g/= :customer-rank 1))))
 
+(g/show customer-segments)
+
 (-> shared-patterns
     (g/group-by :pattern-id)
     (g/agg {:descriptors (g/array-sort (g/collect-set :descriptor))})
     (g/order-by :pattern-id)
     g/show)
+; +----------+---------------------------------------------------------------------------------------------------------------+
+; |pattern-id|descriptors                                                                                                    |
+; +----------+---------------------------------------------------------------------------------------------------------------+
+; |0         |[christmas, decoration, flowr, glass, hanging, heart, holder, shaker, silver, small, tlight, white]            |
+; |1         |[bar, bluepink, discount, draw, fig, goldie, john, orangefuschia, seventeen, shelves, sideboard, tiled]        |
+; |2         |[blokey, bottle, cakestand, charger, delphinium, ginger, hot, jun, occasional, peter, regency, water]          |
+; |3         |[bag, design, jumbo, lunch, necklblue, paisley, pink, postage, red, retrospot, set, shopper]                   |
+; |4         |[bamboo, bertie, burg, chime, cupcandle, lampshade, matches, number, retrodisc, ribs, shapes, tint]            |
+; |5         |[assortment, craft, doll, felt, feltcraft, invites, jewelery, lazer, occasions, princess, trinket, wallet]     |
+; |6         |[adjustment, beadshand, climbing, gin, hydranga, image, jun, metalic, peter, resolution, sign, silvernatural]  |
+; |7         |[boudicca, burst, comb, enamelglass, feathers, glasssilver, jun, midn't, necklace, necklturquois, peter, sil't]|
+; +----------+---------------------------------------------------------------------------------------------------------------+
 
-(-> customer-segments
-    (g/group-by :pattern-id)
-    (g/agg {:n-customers (g/count-distinct :customer-id)})
-    (g/order-by :pattern-id)
-    g/show)
+;(-> customer-segments
+    ;(g/group-by :pattern-id)
+    ;(g/agg {:n-customers (g/count-distinct :customer-id)})
+    ;(g/order-by :pattern-id)
+    ;g/show)
 
 
 (comment
