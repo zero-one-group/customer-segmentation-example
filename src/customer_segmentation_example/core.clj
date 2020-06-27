3c58db86a5690371f3bffc4a0f2c7552d33b1691
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-27 14:44:10 +0700

    Set max-iter to 200

--- src/customer_segmentation_example/core.clj
+++ src/customer_segmentation_example/core.clj
@@ -5,6 +5,7 @@
     [zero-one.geni.ml :as ml]))
 
 (defonce spark (g/create-spark-session {}))
+(.setCheckpointDir (.sparkContext spark) "checkpoint/") ;; TODO: set from map
 
 (def invoices
   (-> (g/read-csv! spark "/data/online_retail_ii")
@@ -17,8 +18,6 @@
          :price        (g/double :Price)
          :customer-id  (g/int "Customer ID")
          :country      :Country})))
-
-(g/count invoices)
 
 (def descriptors
   (-> invoices
@@ -34,8 +33,6 @@
                                                    (g/lit "")))
       (g/remove (g/< (g/length :descriptor) 3))))
 
-(g/show descriptors)
-
 (def log-spending
   (-> descriptors
       (g/remove (g/||
@@ -46,14 +43,13 @@
       (g/agg {:log-spend (g/log1p (g/sum (g/* :price :quantity)))})
       (g/order-by (g/desc :log-spend))
       g/cache))
-
 (g/show log-spending)
 
 (def pipeline
   (ml/pipeline
     (ml/string-indexer {:input-col  :descriptor
                         :output-col :descriptor-id})
-    (ml/als {:max-iter    10
+    (ml/als {:max-iter    200
              :reg-param   0.01
              :rank        8
              :nonnegative true
@@ -81,12 +77,11 @@
                       (g/over (g/row-number)
                               (g/window {:partition-by :pattern-id
                                          :order-by     (g/desc :factor-weight)})))
-      (g/filter (g/< :pattern-rank 13))
+      (g/filter (g/< :pattern-rank 9))
       (g/order-by :pattern-id (g/desc :factor-weight))
       (g/select :pattern-id :descriptor :factor-weight)))
 
 
-;https://stackoverflow.com/questions/31484460/spark-gives-a-stackoverflowerror-when-training-using-als
 (def customer-segments
   (-> (ml/user-factors als-model)
       (g/select (g/as :id :customer-id) (g/posexplode :features))
@@ -97,7 +92,6 @@
                               (g/window {:partition-by :customer-id
                                          :order-by     (g/desc :factor-weight)})))
       (g/filter (g/= :customer-rank 1))))
-
 (g/show customer-segments)
 
 (-> shared-patterns
@@ -105,25 +99,36 @@
     (g/agg {:descriptors (g/array-sort (g/collect-set :descriptor))})
     (g/order-by :pattern-id)
     g/show)
-; +----------+---------------------------------------------------------------------------------------------------------------+
-; |pattern-id|descriptors                                                                                                    |
-; +----------+---------------------------------------------------------------------------------------------------------------+
-; |0         |[christmas, decoration, flowr, glass, hanging, heart, holder, shaker, silver, small, tlight, white]            |
-; |1         |[bar, bluepink, discount, draw, fig, goldie, john, orangefuschia, seventeen, shelves, sideboard, tiled]        |
-; |2         |[blokey, bottle, cakestand, charger, delphinium, ginger, hot, jun, occasional, peter, regency, water]          |
-; |3         |[bag, design, jumbo, lunch, necklblue, paisley, pink, postage, red, retrospot, set, shopper]                   |
-; |4         |[bamboo, bertie, burg, chime, cupcandle, lampshade, matches, number, retrodisc, ribs, shapes, tint]            |
-; |5         |[assortment, craft, doll, felt, feltcraft, invites, jewelery, lazer, occasions, princess, trinket, wallet]     |
-; |6         |[adjustment, beadshand, climbing, gin, hydranga, image, jun, metalic, peter, resolution, sign, silvernatural]  |
-; |7         |[boudicca, burst, comb, enamelglass, feathers, glasssilver, jun, midn't, necklace, necklturquois, peter, sil't]|
-; +----------+---------------------------------------------------------------------------------------------------------------+
+; +----------+------------------------------------------------------------------------+
+; |pattern-id|descriptors                                                             |
+; +----------+------------------------------------------------------------------------+
+; |0         |[eye, hanging, heart, holder, jun, peter, tigris, tlight]               |
+; |1         |[bar, cabin, draw, garld, kashmiri, roccoco, seventeen, sideboard]      |
+; |2         |[clockfuschia, coathangers, hot, jun, peter, pinkblack, rucksack, water]|
+; |3         |[bag, design, jumbo, lunch, pink, red, retrospot, suki]                 |
+; |4         |[bamboo, fig, retrodisc, rnd, scissor, sculpted, shapes, shelves]       |
+; |5         |[afghan, capiz, frutti, lazer, pair, seventeen, sideboard, yellowblue]  |
+; |6         |[cake, ceramic, fairy, hanging, metal, sign, stand, time]               |
+; |7         |[circus, mintivory, necklturquois, paper, parade, regency, set, tin]    |
+; +----------+------------------------------------------------------------------------+
 
-;(-> customer-segments
-    ;(g/group-by :pattern-id)
-    ;(g/agg {:n-customers (g/count-distinct :customer-id)})
-    ;(g/order-by :pattern-id)
-    ;g/show)
-
+(-> customer-segments
+    (g/group-by :pattern-id)
+    (g/agg {:n-customers (g/count-distinct :customer-id)})
+    (g/order-by :pattern-id)
+    g/show)
+; +----------+-----------+
+; |pattern-id|n-customers|
+; +----------+-----------+
+; |0         |659        |
+; |1         |966        |
+; |2         |431        |
+; |3         |370        |
+; |4         |2017       |
+; |5         |621        |
+; |6         |347        |
+; |7         |467        |
+; +----------+-----------+
 
 (comment
   (g/print-schema (g/read-csv! spark "data/online_retail_ii"))
