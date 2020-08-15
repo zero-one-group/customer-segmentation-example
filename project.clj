85e2e79696c165c5535370363c71dc8c3c6c93e0
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-08-15 18:25:30 +0700

    Updated geni deps

--- project.clj
+++ project.clj
@@ -4,22 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.12"]]
+                 [zero.one/geni "0.0.22"]]
   :profiles {:provided
              {:dependencies [;; Spark
+                             [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
                              [org.apache.spark/spark-core_2.12 "3.0.0"]
                              [org.apache.spark/spark-hive_2.12 "3.0.0"]
                              [org.apache.spark/spark-mllib_2.12 "3.0.0"]
                              [org.apache.spark/spark-sql_2.12 "3.0.0"]
                              [org.apache.spark/spark-streaming_2.12 "3.0.0"]
-                             ;; Optional: Spark XGBoost
-                             [ml.dmlc/xgboost4j-spark_2.12 "1.0.0"]
-                             [ml.dmlc/xgboost4j_2.12 "1.0.0"]
-                             ;; Optional: Google Sheets Integration
-                             [com.google.api-client/google-api-client "1.30.9"]
-                             [com.google.apis/google-api-services-drive "v3-rev197-1.25.0"]
-                             [com.google.apis/google-api-services-sheets "v4-rev612-1.25.0"]
-                             [com.google.oauth-client/google-oauth-client-jetty "1.30.6"]
-                             [org.apache.hadoop/hadoop-client "2.7.3"]]}
+                             [org.apache.spark/spark-yarn_2.12 "3.0.0"]]}
              :uberjar {:aot :all}}
   :target-path "target/%s")
